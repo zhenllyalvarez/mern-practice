fa0313ad1f5dcee9fdf5994e06443d6e9908d0af
zhenllyalvarez <dev.zhenllyavarez@gmail.com>
2025-09-24 18:00:14 +0800

    updated controller

--- src/controller/crudRoute.js
+++ src/controller/crudRoute.js
@@ -7,7 +7,23 @@ export async function getAlldata(req, res) {
         res.status(200).json(getmern);
     } catch (error) {
         console.log("Error in getAlldata controller");
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ message: "Internal server error!" });
+    }
+}
+
+export async function getOneData(req, res) {
+    try {
+        const userId = req.params.id;
+        const getData = await mern.findById(userId);
+
+        if(!getData) {
+            return res.status(404).json({ message: "Data not found!" });
+        } else {
+            return res.status(200).json(getData);
+        }
+    } catch (error) {
+        console.log("Error in getOneData controller");
+        res.status(500).json({ message: "Internal server error!"});
     }
 }
 
@@ -18,12 +34,9 @@ export async function createData(req, res) {
         const newData = new mern({ title: title, content: content });
 
         await newData.save();
-        res.status(201).json({ message: "Data Successfully created!" });
-        // if(newData) {
-
-        // } else {
-
-        // }
+        if(newData) {
+            return res.status(201).json({ message: "Data Successfully created!" });
+        }
     } catch (error) {
         console.log("Error in creatingData controller");
         res.status(500).json({ message: "Internal server error" });
@@ -32,23 +45,37 @@ export async function createData(req, res) {
 
 export async function updateData(req, res) {
     // res.status(200).send("Successfully update data!");
-
     try {
         const { title, content } = req.body;
-        const updateData = new mern({ title: title, content: content });
         const userID = req.params.id;
-        const user = await updateData.findByID(userID);
+        const user = await mern.findByIdAndUpdate(
+            userID,
+            { title, content },
+            { new: true },
+        );
 
         if(!user) {
-            res.json({ message: "Failed to update the data!" });
+            return res.status(404).json({ message: "Data not found!" });
         } else {
-            
+            return res.status(200).json({ message: "Successfully updated data!", user });
         }
     } catch (error) {
         res.json({ message: "Internal error in updateData in controller!" });
     }
 }
 
-export function deleteData(req, res) {
-    res.status(200).send("Successfully deleted the data!");
+export async function deleteData(req, res) {
+    // res.status(200).send("Successfully deleted the data!");
+    try {
+        const deletedata = req.params.id;
+        const userData = await mern.findByIdAndDelete(deletedata);
+
+        if(!userData) {
+            return res.status(404).json({ message: "No data found to delete!" });
+        } else {
+            return res.status(200).json({ message: "Successfully deleted the data!" });
+        }
+    } catch (error) {
+        res.json({ message: "Internal error in deleteData in controller!" });
+    }
 }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAlldata, createData, updateData, deleteData } from "../controller/crudRoute.js";
+import { getAlldata, getOneData, createData, updateData, deleteData } from "../controller/crudRoute.js";
 
 const router = express.Router();
 
 router.get("/", getAlldata);
+router.get("/:id", getOneData);
 router.post("/", createData);
 router.post("/:id", updateData);
 router.delete("/:id", deleteData);
